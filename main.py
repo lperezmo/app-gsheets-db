fdc89d51fff5ebf7c34d4c14f77489bb9dedcfd8
Luis Perez Morales <61613934+lperezmo@users.noreply.github.com>
2022-10-21 09:18:02 -0500

    Update main.py
    Overhaul of the main code to make it faster & removed strange variable names (now they are more clear)

--- main.py
+++ main.py
@@ -39,17 +39,10 @@ def check_password():
         return True
 ##################################################################################################################
 if check_password():
-    # IMPORTANT FUNCTIONS
-    
-    # @st.experimental_memo
-    def get_connection_object():
-        """
-        Create a connection object. 
-
-        Note: @st.experimental_singleton is a key-value store that's shared across 
-        all sessions of a Streamlit app. It's great for storing heavyweight singleton 
-        objects across sessions, like database connections.
-        """
+    # HELPER FUNCTIONS
+    @st.experimental_memo
+    def initialize():
+        # Create a connection object.
         credentials = service_account.Credentials.from_service_account_info(
             st.secrets["gcp_service_account"],
             scopes=[
@@ -59,42 +52,21 @@ if check_password():
                     "https://www.googleapis.com/auth/drive"
                     ],
         )
+        # Get sheet
         client = gspread.authorize(credentials)
-        return client
+        sheet = client.open('private-spreadsheet')
 
-    # @st.experimental_memo
-    def get_sheet(name_of_file):
-        """
-        Read database from connection object, get whole spreadsheet
-        """
-        client = get_connection_object()
-        return client.open(name_of_file)
+        # Get current supervisors
+        current_supervisors = []
+        _current_supervisors = sheet.worksheets()
+        for i in _current_supervisors:
+            current_supervisors.append(str(i.title))
 
-    @st.experimental_memo
-    def get_worksheet(supervisor_name, sheet):
-        """
-        Get privileges for given supervisor name. This function
-        gets a worksheet within spreadsheet named after supervisor.
-        """
-        worksheet = sheet.worksheet(supervisor_name)
-        return worksheet
-
-    @st.experimental_memo
-    def get_stored_data(shit_supervisors):
-        # Store all current privileges
+        # Get accesses for each supervisor
         holder = dict()
-        for count, item in enumerate(shit_supervisors):
-            # holder[item] = pd.DataFrame(sheet.worksheet(item).col_values(1)[1:])
-            _ = pd.DataFrame(sheet.worksheet(item).col_values(1))
-            holder[item] = _
-        return holder
-
-    shit_supervisors = []
-    shit_departments = st.secrets['departments']
-    ##################################################################################################################
-    # DATA AND WIDGETS
-    # Title
-    st.header('Privilege assigning program by LPM')
+        for n in current_supervisors:
+            _ = pd.DataFrame(sheet.worksheet(n).col_values(1))
+            holder[n] = _
 
     @st.experimental_memo
     def get_assigned_access():
@@ -127,139 +99,187 @@ if check_password():
             _ = pd.DataFrame(sheet.worksheet(n).col_values(1))
             holder[n] = _
 
+        # Clear memory for reloading data
+        access_to_by_supervisor.clear()
+        get_assigned_access.clear()
+
         return current_supervisors, holder
 
-    current_supervisors, holder = get_assigned_access()
+    @st.experimental_memo
+    def create_supervisor(new_sup):
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                    "https://spreadsheets.google.com/feeds", 
+                    "https://www.googleapis.com/auth/spreadsheets",
+                    "https://www.googleapis.com/auth/drive.file", 
+                    "https://www.googleapis.com/auth/drive"
+                    ],
+        )
+        # conn = connect(credentials=credentials)
+        client = gspread.authorize(credentials)
 
-    # st.table(current_supervisors)
+        # Get sheet
+        sheet = client.open('private-spreadsheet')
+
+        # Add supervisor
+        sheet.add_worksheet(title=new_sup, rows=20, cols=1)
+
+        # Clear memory for reloading data
+        access_to_by_supervisor.clear()
+        get_assigned_access.clear()
+
+    @st.experimental_memo
+    def delete_supervisor(sup_name):
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                    "https://spreadsheets.google.com/feeds", 
+                    "https://www.googleapis.com/auth/spreadsheets",
+                    "https://www.googleapis.com/auth/drive.file", 
+                    "https://www.googleapis.com/auth/drive"
+                    ],
+        )
+
+        # Get sheet
+        client = gspread.authorize(credentials)
+        sheet = client.open('private-spreadsheet')
+
+        # Delete supervisor
+        worksheet = sheet.worksheet(sup_name)
+        sheet.del_worksheet(worksheet)
+        st.success(f"Successfully deleted {sup_name}")
+
+        # Clear memory for reloading data
+        access_to_by_supervisor.clear()
+        get_assigned_access.clear()
+
+    @st.experimental_memo
+    def add_deparment_to_supervisor(sup_name, department_to_add):
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                    "https://spreadsheets.google.com/feeds", 
+                    "https://www.googleapis.com/auth/spreadsheets",
+                    "https://www.googleapis.com/auth/drive.file", 
+                    "https://www.googleapis.com/auth/drive"
+                    ],
+        )
+        # Get sheet
+        client = gspread.authorize(credentials)
+        sheet = client.open('private-spreadsheet')
+        worksheet = sheet.worksheet(sup_name)
+
+        # Add deparment
+        if department_to_add in list(worksheet.col_values(1)):
+            st.warning("Deparment has already been assigned")
+        else:
+            worksheet.insert_row([department_to_add], 1)
+            st.success(f"Deparment {department_to_add} has been assigned to {sup_name}")
+
+        # Clear memory for reloading data
+        access_to_by_supervisor.clear()
+        get_assigned_access.clear()
+
+    @st.experimental_memo
+    def remove_department_from_supervisor(sup_name, dept_to_remove):
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                    "https://spreadsheets.google.com/feeds", 
+                    "https://www.googleapis.com/auth/spreadsheets",
+                    "https://www.googleapis.com/auth/drive.file", 
+                    "https://www.googleapis.com/auth/drive"
+                    ],
+        )
+        # Get sheet
+        client = gspread.authorize(credentials)
+        sheet = client.open('private-spreadsheet')
+        worksheet = sheet.worksheet(sup_name)
+
+        # Delete department to remove
+        cell = worksheet.find(dept_to_remove)
+        worksheet.delete_row(cell.row)
+        st.success(f"Department {dept_to_remove} removed from {sup}'s privileges")
+
+        # Clear memory for reloading data
+        access_to_by_supervisor.clear()
+        get_assigned_access.clear()
+
+    @st.experimental_memo
+    def access_to_by_supervisor(sup_name):
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[
+                    "https://spreadsheets.google.com/feeds", 
+                    "https://www.googleapis.com/auth/spreadsheets",
+                    "https://www.googleapis.com/auth/drive.file", 
+                    "https://www.googleapis.com/auth/drive"
+                    ],
+        )
+        # conn = connect(credentials=credentials)
+        client = gspread.authorize(credentials)
+
+        # Get worksheet
+        sheet = client.open('private-spreadsheet')
+        worksheet = sheet.worksheet(sup_name)
+        list_of_access = worksheet.col_values(1)
+
+        return list_of_access
+
+    ##################################################################################################################
+    # MAIN PROGRAM
+
+    # Title
+    st.header('Privilege assigning program by LPM')
+
+    # Initialize
+    initialize()
+
+    # Get current assigned access to show
+    current_supervisors, holder = get_assigned_access()
+    current_supervisors = sorted(current_supervisors)
     st.subheader("Current privileges")
     for i in holder.keys():
         try:
             tablita = list(holder.get(i).iloc[0])
-            # st.write(i)
-            # st.dataframe(tablita)
-            # for item in tablita:
             st.markdown(f"* {i} has access to {tablita}")
         except:
             pass
-    # Get data
-    sheet = get_sheet("private-spreadsheet")
 
-    # Add already existing supervisors to list
-    worksheet_list = sheet.worksheets()
-    # st.experimental_show(worksheet_list)
+    # Get list of departments from secrets
+    departments = st.secrets['departments']
 
-    # This add a supervisor not in the original list, so you don't have to edit by hand all the time
-    for i in worksheet_list:
-        inner = str(i.title)
-        if inner not in shit_supervisors:
-            shit_supervisors.append(inner)
-            # st.experimental_show(shit_supervisors)
+    # Manage current supervisors (select one at a time)
+    st.subheader('Manage existing supervisors:')
 
-    # Create a worksheet for each of the sups if one doesn't exist
-    for count, item in enumerate(shit_supervisors):
-        try:
-            # Add worksheet with sup name
-            sheet.add_worksheet(title=shit_supervisors[count], rows=20, cols=1)
-        except:
-            pass
-        # st.write(item)
+    # Option for adding department
+    with st.expander('Click here to add a new department to this supervisor'):
+        dept_to_add = st.selectbox('Choose department to be added', options=departments)
+        if st.button('Add department'):
+            add_deparment_to_supervisor(dept_to_add)
 
-        #########################################################
-        # UNCOMMENT TO DELETE ALL WORKSHEETS
-        # sheet.del_worksheet(sheet.worksheet(item))
-        #########################################################
+    # Option for removing supervisor
+    sup =  st.selectbox('Choose supervisor', options=current_supervisors)
+    list_of_access = access_to_by_supervisor()
+    with st.expander("Click here to remove a supervisor"):
+        if st.button('Delete supervisor'):
+            delete_supervisor(sup)
 
-    ##################################################################################################################
-    # ADD NEW SUPERVISOR
+    # Remove privileges
+    with st.expander("Click here to remove a department from this supervisor"):
+        dept_to_remove = st.selectbox('Choose department to remove:', options=list_of_access)
+        if st.button('Delete department'):
+            remove_department_from_supervisor(dept_to_remove)
+
+    # Option for adding supervisor
+    st.subheader('Add new supervisor.')
     with st.expander("Click here to add a new supervisor"):
         new_sup = st.text_input('Name')
         if st.button('Add'):
-            sheet.add_worksheet(title=new_sup, rows=20, cols=1)
-            shit_supervisors.append(new_sup)
-            st.success("New supervisor added successfully")
-
-    st.subheader("Choose supervisor to look at:")
-    sup =  st.selectbox('Choose supervisor', options=shit_supervisors)
-
-    ##################################################################################################################
-    # REMOVE SUPERVISOR
-    with st.expander("Click here to remove a supervisor"):
-        _worksheet = sheet.worksheet(sup)
-        if st.button('Delete supervisor'):
-            sheet.del_worksheet(_worksheet)
-            st.success(f"Successfully deleted {sup}")
-            get_stored_data.clear()
-            st.experimental_rerun()
-
-    ##################################################################################################################
-    # ADD PRIVILEGES
-    with st.expander('Click here to add a new department to this supervisor'):
-        dept = st.selectbox('Choose department to be added', options=shit_departments)
-        if st.button('Add department'):
-            _worksheet = sheet.worksheet(sup)
-            if dept in list(_worksheet.col_values(1)):
-                pass
-                st.warning("Deparment has already been assigned")
-            else:
-                _worksheet.insert_row([dept], 1)
-                st.success(f"Deparment {dept} has been assigned to {sup}")
-            get_stored_data.clear()
-
-    ##################################################################################################################
-    # REMOVE PRIVILES
-    with st.expander("Click here to remove a department from this supervisor"):
-        _worksheet = sheet.worksheet(sup)
-        dept_to_remove = st.selectbox('Choose department to remove:', options=_worksheet.col_values(1))
-        if st.button('Delete department'):
-            cell = _worksheet.find(dept_to_remove)
-            _worksheet.delete_row(cell.row)
-            st.success(f"Department {dept_to_remove} removed from {sup}'s privileges")
-            get_stored_data.clear()
-
-    ##################################################################################################################
-    # SHOW CURRENT PRIVILEGES IN TWO COLUMNS
-#     # Show data
-#     st.subheader("Currently assigned privileges:")
-#     holder = get_stored_data(shit_supervisors)
-#     col1, col2 = st.columns(2)
-#     for count, i in enumerate(holder.keys()):
-#         if count %2 == 0:
-#             with col1: 
-#                 st.write(i)
-#                 st.dataframe(holder.get(i), use_container_width=True)
-#         else:
-#             with col2:
-#                 st.write(i)
-#                 st.dataframe(holder.get(i), use_container_width=True)
-
-
-
-# import streamlit as st
-# from google.oauth2 import service_account
-# from gsheetsdb import connect
-
-# # Create a connection object.
-# credentials = service_account.Credentials.from_service_account_info(
-#     st.secrets["gcp_service_account"],
-#     scopes=[
-#         "https://www.googleapis.com/auth/spreadsheets",
-#     ],
-# )
-# conn = connect(credentials=credentials)
-
-# # Perform SQL query on the Google Sheet.
-# # Uses st.cache to only rerun when the query changes or after 10 min.
-# @st.cache(ttl=600)
-# def run_query(query):
-#     rows = conn.execute(query, headers=1)
-#     rows = rows.fetchall()
-#     return rows
-
-# sheet_url = st.secrets["private_gsheets_url"]
-# rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# # Print results.
-# for row in rows:
-# #     st.write(f"{row.supervisor} has access to :{row.department}:")
-#     st.write(row)
+            create_supervisor(new_sup)
